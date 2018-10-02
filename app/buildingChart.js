1972401cf523c73fdab7f695a8bafa4e2662abb3
d.shulha <d.shulha@soft-industry.com>
2018-10-02 10:24:13 +0300

    changed entry point (index.js)

--- app/buildingChart.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (data) {
-  for (let row of data) {
-    console.log(row[0] + ' ' + '█'.repeat(Number(row[1])));
-  }
-};
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-require('./app/index');
+const cliParams = require('./app/parsingParams');
+const fetchingData = require('./app/fetchingData');
+
+(async () => {
+  try {
+    let result = await fetchingData(cliParams);
+    ((data) => {
+      for (let row of data) {
+        console.log(row[0] + ' ' + '█'.repeat(Number(row[1])));
+      }
+    })(result);
+  } catch (err) {
+    console.log(err);
+  }
+})();
